8358d2e6f931ba6193e4ccccd0ff852ef12b7cdd
RJ Robinson <rj@rjrobinson.net>
2022-12-29 06:21:55 +0000

    clean up and comment

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,20 @@ const PORT = process.env.PORT
 const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
-
-connectDB()
-
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 
+// Connect to MongoDB Database
+connectDB()
 
-app.get('/',(req,res) => {
-    res.send("heelo")
-})
 
+
+// User routes
 app.use('/api/users', require('./routes/userRoutes'))
 
+// Error middleware
 app.use(errorHandler)
 
+// Start server
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
